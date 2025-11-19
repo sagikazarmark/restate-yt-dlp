097ec1900da0cc2c38ba466d58a2e49686f91aec
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2025-11-19 01:23:26 +0100

    change default service name
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- src/restate_yt_dlp/restate.py
+++ src/restate_yt_dlp/restate.py
@@ -7,7 +7,7 @@ from .downloader import Downloader, DownloadRequest
 
 def create_service(
     downloader: Downloader,
-    service_name: str = "YoutubeDownloader",
+    service_name: str = "yt-dlp",
 ) -> restate.Service:
     service = restate.Service(service_name)
 
@@ -20,7 +20,7 @@ def register_service(
     downloader: Downloader,
     service: restate.Service | None = None,
 ):
-    service = service or restate.Service("YoutubeDownloader")
+    service = service or restate.Service("yt-dlp")
 
     @service.handler()
     async def download(ctx: restate.Context, request: DownloadRequest):
